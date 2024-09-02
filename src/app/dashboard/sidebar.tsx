193f320348f4a2f803372e2e19a6c55525ca6337
JohnzExor <jimenojohnzyll@gmail.com>
2024-09-02 23:23:09 +0800

    improved sidebar

--- src/app/dashboard/sidebar.tsx
+++ src/app/dashboard/sidebar.tsx
@@ -1,7 +1,14 @@
 "use client";
 
 import clsx from "clsx";
-import { BringToFront, Flag, LayoutDashboard, Settings } from "lucide-react";
+import {
+  Book,
+  BringToFront,
+  CircleUser,
+  Flag,
+  LayoutDashboard,
+  Settings,
+} from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
@@ -9,6 +16,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { useSession } from "next-auth/react";
 import Logout from "@/components/logout";
 import { ModeToggle } from "@/components/theme/mode-toggle";
+import { Separator } from "@/components/ui/separator";
 
 const navigations = [
   {
@@ -17,9 +25,19 @@ const navigations = [
     icon: <LayoutDashboard />,
   },
   {
+    path: "/dashboard/account",
+    name: "Account",
+    icon: <CircleUser />,
+  },
+  {
     path: "/dashboard/report-progress",
     name: "Report progress",
     icon: <Flag />,
+  },
+  {
+    path: "/dashboard/handbook",
+    name: "Student Handbook",
+    icon: <Book />,
   },
   {
     path: "/dashboard/settings",
@@ -32,31 +50,34 @@ const SideBar = () => {
   const pathname = usePathname();
   const { data } = useSession();
   return (
-    <aside className=" p-8 w-full max-w-[300px] space-y-8">
-      <label className="font-semibold flex items-center text-2xl gap-2">
-        <BringToFront size={40} />
+    <aside className=" w-80 p-3 py-10 border-r flex flex-col gap-8 h-full">
+      <label className="flex items-center text-xl gap-2">
+        <BringToFront size={30} className="text-primary" />
         Seequentinel
       </label>
       <div className="flex items-center gap-2">
         <Avatar>
-          <AvatarImage src="https://github.com/shadcn.png" />
-          <AvatarFallback>CN</AvatarFallback>
+          <AvatarFallback>
+            {data?.user.email?.slice(0, 2).toUpperCase()}
+          </AvatarFallback>
         </Avatar>
         <div className="flex flex-col">
-          <label className=" font-medium">{data?.user.email}</label>
-          <span className=" text-muted-foreground text-sm">
+          <label className=" font-medium text-sm">{data?.user.email}</label>
+          <span className=" text-muted-foreground text-xs">
             ID: {data?.user.id}
           </span>
         </div>
       </div>
-
-      <div className="flex flex-col gap-4">
+      <Separator />
+      <div className="flex flex-col gap-2 text-sm">
+        <span className=" text-sm text-muted-foreground">Navigations</span>
         {navigations.map(({ path, name, icon }) => (
           <Link
             href={path}
-            className={clsx("flex items-center gap-2", {
-              "bg-primary w-full p-2 font-semibold text-white rounded-2xl":
-                pathname == path,
+            className={clsx("flex gap-2 p-2 items-center rounded-2xl", {
+              "bg-primary w-full text-white ": pathname === path,
+              "hover:bg-black hover:bg-opacity-5 duration-100":
+                pathname !== path,
             })}
           >
             {icon}
@@ -65,7 +86,7 @@ const SideBar = () => {
         ))}
       </div>
 
-      <div>
+      <div className=" mt-auto flex items-center justify-between text-sm px-4">
         <Logout />
         <ModeToggle />
       </div>
--- src/components/logout.tsx
+++ src/components/logout.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { LogOut } from "lucide-react";
 import { signOut } from "next-auth/react";
 
 const Logout = () => {
@@ -7,9 +8,10 @@ const Logout = () => {
     await signOut();
   };
   return (
-    <div>
-      <button onClick={handleLogout}>Logout</button>
-    </div>
+    <button onClick={handleLogout} className="text-sm flex items-center gap-2">
+      <LogOut />
+      Logout
+    </button>
   );
 };
 
