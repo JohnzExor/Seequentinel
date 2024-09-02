bd4b09edd7b15d5ef5efb98c02b8da4cb5d6f012
JohnzExor <jimenojohnzyll@gmail.com>
2024-09-02 23:23:09 +0800

    improved sidebar

--- src/app/dashboard/sidebar-toggle.tsx
+++ src/app/dashboard/sidebar-toggle.tsx
@@ -3,7 +3,7 @@ import SideBar from "./sidebar";
 const SideBarToggle = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex">
-      <div className="hidden md:block">
+      <div className="hidden md:block h-screen">
         <SideBar />
       </div>
       {children}
