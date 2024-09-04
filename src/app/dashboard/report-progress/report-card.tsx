2d3fd733899e55d4cc572512ea48c354b4faf8ba
JohnzExor <jimenojohnzyll@gmail.com>
2024-09-04 16:24:24 +0800

    added stepper on report progress

--- src/app/dashboard/report-progress/report-card.tsx
+++ src/app/dashboard/report-progress/report-card.tsx
@@ -1,5 +1,12 @@
 import { FaultyFacilities } from "@prisma/client";
-import { CircleFadingArrowUp, TriangleAlert } from "lucide-react";
+import {
+  BookCheck,
+  CircleFadingArrowUp,
+  ListCheck,
+  Loader,
+  Square,
+  TriangleAlert,
+} from "lucide-react";
 import {
   Dialog,
   DialogContent,
@@ -11,6 +18,35 @@ import {
 import { ReactElement } from "react";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import clsx from "clsx";
+import { Separator } from "@/components/ui/separator";
+
+const steps = [
+  {
+    name: "Request",
+    description:
+      "Provide your name and email address. We will use this information to create your account",
+    icon: <Loader />,
+  },
+  {
+    name: "Reviewing",
+    description:
+      "Provide your name and email address. We will use this information to create your account",
+    icon: <Square />,
+  },
+  {
+    name: "Accepted",
+    description:
+      "Provide your name and email address. We will use this information to create your account",
+    icon: <ListCheck />,
+  },
+  {
+    name: "Completed",
+    description:
+      "Provide your name and email address. We will use this information to create your account",
+    icon: <BookCheck />,
+  },
+];
 
 const ReportCard = async ({
   title,
@@ -62,12 +98,49 @@ const ReportCard = async ({
                     <span>Created: {createdAt.toDateString()}</span>
                   </DialogDescription>
                   <div>
-                    <div className="flex items-center gap-3 p-4">
-                      <h1 className="text-muted-foreground text-sm">Status:</h1>
-                      <p className=" flex items-center gap-1 text-sm font-bold">
-                        <CircleFadingArrowUp size={20} />
-                        {status}
-                      </p>
+                    <div className="flex flex-col gap-4 py-4">
+                      {steps.map(({ name, description, icon }, index) => (
+                        <div
+                          key={index}
+                          className={clsx("flex items-start gap-4", {
+                            " text-muted-foreground":
+                              index >
+                              steps.findIndex((obj) => obj.name === status),
+                          })}
+                        >
+                          <div className="flex items-center flex-col">
+                            <div
+                              className={clsx(
+                                " w-fit p-3 rounded-full text-white",
+                                {
+                                  " bg-primary": status === name,
+                                  " bg-muted": status !== name,
+                                  " text-current":
+                                    index <=
+                                    steps.findIndex(
+                                      (obj) => obj.name === status
+                                    ) -
+                                      1,
+                                }
+                              )}
+                            >
+                              {icon}
+                            </div>
+                            <Separator
+                              className={clsx("rotate-90 w-10 mt-5 mb-1", {
+                                hidden: index >= steps.length - 1,
+                              })}
+                            />
+                          </div>
+
+                          <p className=" font-semibold flex flex-col text-left">
+                            {name}
+                            <span className=" text-sm text-muted-foreground font-normal">
+                              {description}
+                            </span>
+                          </p>
+                        </div>
+                      ))}
                     </div>
                     <div>
                       <span className="text-sm text-muted-foreground">
