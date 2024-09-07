4aa960d34ad131d17926c562ccf2cc9201b81484
Huemman <mehmehkyuto@gmail.com>
2024-09-07 07:34:02 -0800

    add text description to report options

--- src/app/report/(report)/page.tsx
+++ src/app/report/(report)/page.tsx
@@ -19,16 +19,20 @@ import clsx from "clsx";
 
 const reportOptions = [
   {
-    name: "Faulty Facilities",
+    name: "Campus Maintenance Request",
     link: "/report/faulty-facilities",
     icon: <ConstructionIcon />,
     image: "/images/psu-front.webp",
+    description:
+      "This allows students, faculty, and staff to report faulty utilities, broken equipment, or hazardous conditions on campus.",
   },
   {
-    name: "Behavioral",
+    name: "Handbook Violation Report",
     link: "/report/behavioral",
     icon: <Book />,
     image: "/images/students.jpg",
+    description:
+      "This allows students, faculty, and staff to report a violation against the University guidelines or student handbook on campus.",
   },
   {
     name: "Emergency",
@@ -40,7 +44,7 @@ const reportOptions = [
 const page = () => {
   return (
     <div className="flex flex-col p-2 justify-start w-full">
-      {reportOptions.map(({ name, link, icon, image }, index) => (
+      {reportOptions.map(({ name, link, icon, image, description }, index) => (
         <Link href={link} key={index}>
           <Card className="h-48 m-[10px] relative">
             {image ? (
@@ -55,19 +59,29 @@ const page = () => {
                 </div>
               </div>
             ) : null}
-            <CardContent className="flex items-center p-4 z-10">
-              <p
-                className={clsx(
-                  " z-20 flex items-center gap-2 text-2xl font-semibold",
-                  {
-                    "text-white": image,
-                  }
-                )}
+            <CardHeader>
+              <CardTitle className="flex items-center p-4 z-10">
+                <p
+                  className={clsx(
+                    " z-20 flex items-center gap-2 text-2xl font-semibold",
+                    {
+                      "text-white": image,
+                    }
+                  )}
+                >
+                  {icon}
+                  {name}
+                </p>
+              </CardTitle>
+              <CardDescription
+                className={clsx("mt-2 z-10", {
+                  "text-gray-200": image,
+                  "text-gray-600": !image,
+                })}
               >
-                {icon}
-                {name}
-              </p>
-            </CardContent>
+                {description}
+              </CardDescription>
+            </CardHeader>
           </Card>
         </Link>
       ))}
